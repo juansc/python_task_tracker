b82125c585a737243ef8a3115debacfdf61dcaaa
Juan S Carrillo <juanscarrillo23@gmail.com>
2015-12-05 20:56:34 -0800

    Data is now written to a csv.

--- main.py
+++ main.py
@@ -13,31 +13,61 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+
 class taskNotification(QtGui.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
-        # Explaining super is out of the scope of this article
-        # So please google it if you're not familar with it
-        # Simple reason why we use it here is that it allows us to
-        # access variables, methods etc in the design.py file
-        self.file_name = None
-        self.string_to_match = None
+        self.data = []
+        self.modified_rows = []
+        self.csv_file = 'tool_files.csv'
+        self.target_file = 'test.csv'
+
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
-        # It sets up layout and widgets that are defined
-        self.setup_table()
 
-    def setup_table(self):
-        num_of_cols = 6
+        self.setup_table()
+        self.setup_connections()
+        self.table.cellChanged.connect(self.data_has_been_changed)
+
+    def setup_connections(self):
+        self.table.cellChanged.connect(self.data_has_been_changed)
+        self.cancel_btn.clicked.connect(self.close)
+        self.save_btn.clicked.connect(self.save_changes)
+
+    def close_app(self):
+        self.close()
+
+
+    def data_has_been_changed(self):
+        modified_row = self.table.currentRow()
+        row_contents = []
+
+        for col in range(0, self.table.columnCount()):
+            text = str(self.table.item(modified_row, col).text())
+            row_contents.append(text)
+
+        is_row_changed = sum(i != j for i, j in zip(row_contents, self.data[modified_row])) > 0
+        self.modified_rows[modified_row] = is_row_changed
+
+    def read_csv_file(self):
         tasks = []
-        with open('tool_files.csv', 'rb') as csvfile:
+        with open(self.csv_file, 'rb') as csvfile:
             file_reader = csv.reader(csvfile, delimiter=',')
             for row in file_reader:
                 tasks.append(row)
+        return tasks
 
+    def setup_table(self):
+        tasks = self.read_csv_file()
+
+        self.data = tasks[1:]
+
+        number_of_columns = len(tasks[0])
         number_of_tasks = len(tasks) - 1
+        self.modified_rows = [False] * number_of_tasks
         self.table.setRowCount(number_of_tasks)
+
         for row in range(1, number_of_tasks + 1):
-            for col in range(0, num_of_cols):
+            for col in range(0, number_of_columns):
                 item = QtGui.QTableWidgetItem()
                 self.table.setItem(row - 1, col, item)
                 item.setText(_translate("MainWindow", tasks[row][col], None))
@@ -49,9 +79,38 @@ class taskNotification(QtGui.QMainWindow, design.Ui_MainWindow):
                 item = self.table.item(row, col)
                 item.setFlags(QtCore.Qt.ItemIsEnabled)
 
+    def save_changes(self):
+        self.write_to_csv_file()
+        self.send_email()
+
+    def write_to_csv_file(self):
+        data = self.get_data_from_table()
+        headers = ['Asset Name','Owner','Status','Notes','Due Date','Requester']
+        data.insert(0, headers)
+        with open(self.target_file, 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
 
 
 
+    def get_data_from_table(self):
+        data = []
+        row_data = []
+        num_of_rows = self.table.rowCount()
+        num_of_cols = self.table.columnCount()
+
+        for row in range(0, num_of_rows):
+            row_data = []
+            for col in range(0, num_of_cols):
+                row_data.append(str(self.table.item(row, col).text()))
+            data.append(row_data)
+
+        print data
+        return data
+
+
+    def send_email(self):
+        print "Pop up window should appear"
 
 def main():
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
